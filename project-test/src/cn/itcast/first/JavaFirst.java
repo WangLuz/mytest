3154baff51ade31d9ea9c27c575b3ba52028c2dc
itwz <wlz9638@163.com>
2017-06-05 23:30:02 +0800

    第二次修改提交

--- project-test/src/cn/itcast/first/JavaFirst.java
+++ project-test/src/cn/itcast/first/JavaFirst.java
@@ -8,5 +8,6 @@ public class JavaFirst {
 	public void testHelloWrold() {
 		System.out.println("HelloWorld!");
 		System.out.println("helloWorld!111111111");
+		System.out.println("helloWorld!22222222222");
 	}
 }
